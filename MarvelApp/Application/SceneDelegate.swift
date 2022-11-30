6c065b85e19860ba4ee489cd14cd860977976f79
i40per <l40perl@gmail.com>
2022-12-01 00:42:18 +0600

    Refactoring code

--- MarvelApp/Application/SceneDelegate.swift
+++ MarvelApp/Application/SceneDelegate.swift
@@ -2,7 +2,7 @@
 ////  SceneDelegate.swift
 ////  MarvelApp
 ////
-////  Created by MacBook on 18.11.2022.
+////  Created by Evgenii Lukin on 18.11.2022.
 ////
 //
 //import UIKit
@@ -10,7 +10,6 @@
 //class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //
 //    var window: UIWindow?
-//
 //
 //    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 //        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +45,5 @@
 //        // Use this method to save data, release shared resources, and store enough scene-specific state information
 //        // to restore the scene back to its current state.
 //    }
-//
-//
 //}
 //
